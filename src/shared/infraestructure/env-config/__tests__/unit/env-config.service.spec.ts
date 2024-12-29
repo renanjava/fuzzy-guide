36cbc53500fd37aa2c03ea1f4bb810a6113cdba8
Renan <renancvr777@gmail.com>
2024-12-29 04:49:58 -0300

    fix: eslint e prettier

--- src/shared/infraestructure/env-config/__tests__/unit/env-config.service.spec.ts
+++ src/shared/infraestructure/env-config/__tests__/unit/env-config.service.spec.ts
@@ -1,31 +1,31 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { EnvConfigService } from '../../env-config.service';
-import { EnvConfigModule } from '../../env-config.module';
+import { Test, TestingModule } from '@nestjs/testing'
+import { EnvConfigService } from '../../env-config.service'
+import { EnvConfigModule } from '../../env-config.module'
 
 describe('EnvConfigService unit tests', () => {
-  let sut: EnvConfigService;
+  let sut: EnvConfigService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [EnvConfigModule.forRoot()],
       providers: [EnvConfigService],
-    }).compile();
+    }).compile()
 
-    sut = module.get<EnvConfigService>(EnvConfigService);
-  });
+    sut = module.get<EnvConfigService>(EnvConfigService)
+  })
   describe('enviroments variables tests', () => {
     it('should return PORT variable', () => {
-      expect(sut.getAppPort()).toBe(3000);
-    });
+      expect(sut.getAppPort()).toBe(3000)
+    })
 
     it('should return NODE_ENV variable', () => {
-      expect(sut.getNodeEnv()).toBe('test');
-    });
-  });
+      expect(sut.getNodeEnv()).toBe('test')
+    })
+  })
 
   describe('instance test', () => {
     it('should be defined', () => {
-      expect(sut).toBeDefined();
-    });
-  });
-});
+      expect(sut).toBeDefined()
+    })
+  })
+})
--- src/shared/infraestructure/env-config/env-config.module.ts
+++ src/shared/infraestructure/env-config/env-config.module.ts
@@ -1,7 +1,7 @@
-import { DynamicModule, Module } from '@nestjs/common';
-import { EnvConfigService } from './env-config.service';
-import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
-import { join } from 'node:path';
+import { DynamicModule, Module } from '@nestjs/common'
+import { EnvConfigService } from './env-config.service'
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config'
+import { join } from 'node:path'
 
 @Module({
   providers: [EnvConfigService],
@@ -13,6 +13,6 @@ export class EnvConfigModule extends ConfigModule {
       envFilePath: [
         join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
       ],
-    });
+    })
   }
 }
