afd78660e6efa4b5fa9d309e79f70e3fc48b77a1
Renan <renancvr777@gmail.com>
2024-12-29 19:27:19 -0300

    add: atributos da CampanhaEntity e testes

---
+++ src/campanha/domain/entities/__tests__/unit/campanha.entity.spec.ts
@@ -0,0 +1,75 @@
+import { faker } from '@faker-js/faker'
+import { CampanhaEntity, CampanhaProps } from '../../campanha.entity'
+
+describe('CampanhaEntity unit tests', () => {
+  let props: CampanhaProps
+  let sut: CampanhaEntity
+
+  beforeEach(() => {
+    props = {
+      qtdBilhetesTotais: faker.number.int(),
+      dataInicioCampanha: faker.date.recent(),
+      campanhaAtiva: true,
+      valorUnitarioBilhete: faker.number.float(),
+    }
+    sut = new CampanhaEntity(props)
+  })
+  it('constructor method', () => {
+    expect(sut.props.qtdBilhetesTotais).toEqual(props.qtdBilhetesTotais)
+    expect(sut.props.dataInicioCampanha).toEqual(props.dataInicioCampanha)
+    expect(sut.props.campanhaAtiva).toEqual(props.campanhaAtiva)
+    expect(sut.props.valorUnitarioBilhete).toEqual(props.valorUnitarioBilhete)
+    expect(sut.props.qtdBilhetesComprados).toEqual(0)
+    expect(sut.props.dataFimCampanha).toBeNull()
+    expect(sut.props.porcentagemProgresso).toEqual(0)
+  })
+
+  it('getter qtdBilhetesTotais', () => {
+    expect(sut.qtdBilhetesTotais).toEqual(props.qtdBilhetesTotais)
+    expect(sut.props.qtdBilhetesTotais).toBeDefined()
+    expect(sut.props.qtdBilhetesTotais).toEqual(props.qtdBilhetesTotais)
+    expect(typeof sut.props.qtdBilhetesTotais).toBe('number')
+  })
+
+  it('getter dataInicioCampanha', () => {
+    expect(sut.dataInicioCampanha).toEqual(props.dataInicioCampanha)
+    expect(sut.props.dataInicioCampanha).toBeDefined()
+    expect(sut.props.dataInicioCampanha).toEqual(props.dataInicioCampanha)
+    expect(sut.props.dataInicioCampanha).toBeInstanceOf(Date)
+  })
+
+  it('getter campanhaAtiva', () => {
+    expect(sut.campanhaAtiva).toEqual(props.campanhaAtiva)
+    expect(sut.props.campanhaAtiva).toBeDefined()
+    expect(sut.props.campanhaAtiva).toEqual(props.campanhaAtiva)
+    expect(typeof sut.props.campanhaAtiva).toBe('boolean')
+  })
+
+  it('getter valorUnitarioBilhete', () => {
+    expect(sut.valorUnitarioBilhete).toEqual(props.valorUnitarioBilhete)
+    expect(sut.props.valorUnitarioBilhete).toBeDefined()
+    expect(sut.props.valorUnitarioBilhete).toEqual(props.valorUnitarioBilhete)
+    expect(typeof sut.props.valorUnitarioBilhete).toBe('number')
+  })
+
+  it('getter qtdBilhetesComprados', () => {
+    expect(sut.qtdBilhetesComprados).toEqual(0)
+    expect(sut.props.qtdBilhetesComprados).toBeDefined()
+    expect(sut.props.qtdBilhetesComprados).toEqual(props.qtdBilhetesComprados)
+    expect(typeof sut.props.qtdBilhetesComprados).toBe('number')
+  })
+
+  it('getter dataFimCampanha', () => {
+    expect(sut.dataFimCampanha).toBeNull()
+    expect(sut.props.dataFimCampanha).toBeDefined()
+    expect(sut.props.dataFimCampanha).toEqual(props.dataFimCampanha)
+    expect(sut.props.dataFimCampanha).toBeNull()
+  })
+
+  it('getter porcentagemProgresso', () => {
+    expect(sut.porcentagemProgresso).toEqual(0)
+    expect(sut.props.porcentagemProgresso).toBeDefined()
+    expect(sut.props.porcentagemProgresso).toEqual(props.porcentagemProgresso)
+    expect(typeof sut.props.porcentagemProgresso).toBe('number')
+  })
+})
--- src/campanha/domain/entities/campanha.entity.ts
+++ src/campanha/domain/entities/campanha.entity.ts
@@ -1,1 +1,45 @@
-export class CampanhaEntity {}
+export type CampanhaProps = {
+  qtdBilhetesTotais: number
+  qtdBilhetesComprados?: number
+  dataInicioCampanha: Date
+  dataFimCampanha?: Date
+  porcentagemProgresso?: number
+  campanhaAtiva: boolean
+  valorUnitarioBilhete: number
+}
+
+export class CampanhaEntity {
+  constructor(public readonly props: CampanhaProps) {
+    this.props.qtdBilhetesComprados = this.props.qtdBilhetesComprados ?? 0
+    this.props.dataFimCampanha = this.props.dataFimCampanha ?? null
+    this.props.porcentagemProgresso = this.props.porcentagemProgresso ?? 0
+  }
+
+  get qtdBilhetesTotais(): number {
+    return this.props.qtdBilhetesTotais
+  }
+
+  get qtdBilhetesComprados(): number {
+    return this.props.qtdBilhetesComprados
+  }
+
+  get dataInicioCampanha(): Date {
+    return this.props.dataInicioCampanha
+  }
+
+  get dataFimCampanha(): Date {
+    return this.props.dataFimCampanha
+  }
+
+  get porcentagemProgresso(): number {
+    return this.props.porcentagemProgresso
+  }
+
+  get campanhaAtiva(): boolean {
+    return this.props.campanhaAtiva
+  }
+
+  get valorUnitarioBilhete(): number {
+    return this.props.valorUnitarioBilhete
+  }
+}
