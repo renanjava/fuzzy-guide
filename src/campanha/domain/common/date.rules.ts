a5d4f5b95f6a6987abb750bdd77680fc3dbbfa5c
Renan <renancvr777@gmail.com>
2025-01-07 16:16:10 -0300

    test: validando mais atributos da CampanhaValidator

--- src/campanha/domain/common/date.rules.ts
+++ src/campanha/domain/common/date.rules.ts
@@ -3,4 +3,5 @@ export const DateRules = {
   MAX_TODAY: new Date(new Date().setHours(23, 59, 59, 999)),
   TOMORROW: new Date(new Date().setDate(new Date().getDate() + 1)),
   YESTERDAY: new Date(new Date().setDate(new Date().getDate() - 1)),
+  TODAY: new Date(),
 }
--- src/campanha/domain/validators/__tests__/unit/campanha.validator.spec.ts
+++ src/campanha/domain/validators/__tests__/unit/campanha.validator.spec.ts
@@ -5,6 +5,7 @@ import {
   CampanhaValidatorFactory,
 } from '../../campanha.validator'
 import { DateRules } from '@/campanha/domain/common/date.rules'
+import { DomainRules } from '@/campanha/domain/common/domain.rules'
 //import { CampanhaProps } from '@/campanha/domain/entities/campanha.entity'
 
 let sut: CampanhaValidator
@@ -255,6 +256,17 @@ describe('CampanhaValidator Unit tests', () => {
       ])
     })
 
+    it('should return error when dataInicioCampanha is a future date', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        dataInicioCampanha: DateRules.TOMORROW,
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['dataInicioCampanha']).toStrictEqual([
+        `maximal allowed date for dataInicioCampanha is ${DateRules.MAX_TODAY}`,
+      ])
+    })
+
     it('should return error when dataInicioCampanha is an invalid date', () => {
       const isValid = sut.validate({
         ...CampanhaDataBuilder({}),
@@ -284,17 +296,210 @@ describe('CampanhaValidator Unit tests', () => {
 
     /*
     it('should return valid when dataInicioCampanha is a valid date', () => {
-      const dataInicioCampanha = new Date()
-
       const isValid = sut.validate({
         ...props,
-        dataInicioCampanha: dataInicioCampanha,
+        dataInicioCampanha: DateRules.TODAY,
       })
       expect(isValid).toBeTruthy()
       expect(sut.validatedData).toStrictEqual(
-        new CampanhaRules({ ...props, dataInicioCampanha: dataInicioCampanha }),
+        new CampanhaRules({ ...props, dataInicioCampanha: DateRules.TODAY }),
       )
     })
     */
+  })
+
+  describe('DataFimCampanha Field', () => {
+    it('should return error when dataFimCampanha is yesterday', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        dataFimCampanha: DateRules.YESTERDAY,
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['dataFimCampanha']).toStrictEqual([
+        `minimal allowed date for dataFimCampanha is ${DateRules.TOMORROW}`,
+      ])
+    })
+
+    it('should return error when dataFimCampanha is today', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        dataFimCampanha: DateRules.TODAY,
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['dataFimCampanha']).toStrictEqual([
+        `minimal allowed date for dataFimCampanha is ${DateRules.TOMORROW}`,
+      ])
+    })
+
+    it('should return error when dataFimCampanha is an invalid date', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        dataFimCampanha: 'invalid-date' as any,
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['dataFimCampanha']).toStrictEqual([
+        `minimal allowed date for dataFimCampanha is ${DateRules.TOMORROW}`,
+        'dataFimCampanha must be a Date instance',
+      ])
+    })
+
+    /*
+    it('should return valid when dataFimCampanha is a future date', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        dataFimCampanha: DateRules.TOMORROW,
+      })
+      expect(isValid).toBeTruthy()
+      expect(sut.validatedData).toStrictEqual(
+        new CampanhaRules({ ...props, dataFimCampanha: DateRules.TOMORROW }
+      )
+    })
+
+    it('should return valid when dataFimCampanha is null', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        dataFimCampanha: null as any,
+      })
+      expect(isValid).toBeTruthy()
+      expect(sut.validatedData).toStrictEqual(
+        new CampanhaRules({ ...props, dataFimCampanha: null }
+      )
+    })
+    */
+  })
+
+  describe('PorcentagemProgresso Field', () => {
+    it('should return error when porcentagemProgresso is below the minimum value', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        porcentagemProgresso: DomainRules.MIN_PROGRESS_PERCENTAGE - 1,
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['porcentagemProgresso']).toStrictEqual([
+        `porcentagemProgresso must not be less than ${DomainRules.MIN_PROGRESS_PERCENTAGE}`,
+      ])
+    })
+
+    it('should return error when porcentagemProgresso is above the maximum value', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        porcentagemProgresso: DomainRules.MAX_PROGRESS_PERCENTAGE + 1,
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['porcentagemProgresso']).toStrictEqual([
+        `porcentagemProgresso must not be greater than ${DomainRules.MAX_PROGRESS_PERCENTAGE}`,
+      ])
+    })
+
+    it('should return error when porcentagemProgresso is not a number', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        porcentagemProgresso: 'string' as any,
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['porcentagemProgresso']).toStrictEqual([
+        'porcentagemProgresso must be a number conforming to the specified constraints',
+        `porcentagemProgresso must not be greater than ${DomainRules.MAX_PROGRESS_PERCENTAGE}`,
+        `porcentagemProgresso must not be less than ${DomainRules.MIN_PROGRESS_PERCENTAGE}`,
+      ])
+    })
+
+    it('should return error when porcentagemProgresso has more than 2 decimal places', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        porcentagemProgresso: 12.345,
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['porcentagemProgresso']).toStrictEqual([
+        'porcentagemProgresso must be a number conforming to the specified constraints',
+      ])
+    })
+
+    /*
+    it('should return valid when porcentagemProgresso is within the valid range and is a valid number', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        porcentagemProgresso: 50.0,
+      })
+      expect(isValid).toBeTruthy()
+    })
+    */
+  })
+
+  describe('CampanhaAtiva Field', () => {
+    it('should return error when campanhaAtiva is not a boolean', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        campanhaAtiva: 'true' as any,
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['campanhaAtiva']).toStrictEqual([
+        'campanhaAtiva must be a boolean value',
+      ])
+    })
+
+    /*
+    it('should return valid when campanhaAtiva is true', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        campanhaAtiva: true,
+      })
+      expect(isValid).toBeTruthy()
+    })
+
+    it('should return valid when campanhaAtiva is false', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        campanhaAtiva: false,
+      })
+      expect(isValid).toBeTruthy()
+    })
+    */
+  })
+
+  describe('ValorUnitarioBilhete Field', () => {
+    it('should return error when valorUnitarioBilhete is not a number', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        valorUnitarioBilhete: 'string' as any,
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['valorUnitarioBilhete']).toStrictEqual([
+        'valorUnitarioBilhete must be a number conforming to the specified constraints',
+      ])
+    })
+
+    it('should return error when valorUnitarioBilhete has more than 2 decimal places', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        valorUnitarioBilhete: 12.345,
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['valorUnitarioBilhete']).toStrictEqual([
+        'valorUnitarioBilhete must be a number conforming to the specified constraints',
+      ])
+    })
+
+    /*
+    it('should return valid when valorUnitarioBilhete is a valid number', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        valorUnitarioBilhete: 10.5,
+      })
+      expect(isValid).toBeTruthy()
+    })
+    */
+
+    it('should return error when valorUnitarioBilhete is empty', () => {
+      const isValid = sut.validate({
+        ...CampanhaDataBuilder({}),
+        valorUnitarioBilhete: null as any,
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['valorUnitarioBilhete']).toStrictEqual([
+        'valorUnitarioBilhete should not be empty',
+        'valorUnitarioBilhete must be a number conforming to the specified constraints',
+      ])
+    })
   })
 })
