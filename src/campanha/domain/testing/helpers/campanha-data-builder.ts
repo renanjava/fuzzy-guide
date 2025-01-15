3bf575ffdd230bf8e8d5e0e882d9d357b4e94eb3
Renan <renancvr777@gmail.com>
2025-01-15 15:39:02 -0300

    add: regras de domínio no databuilder da Campanha

--- src/campanha/domain/testing/helpers/campanha-data-builder.ts
+++ src/campanha/domain/testing/helpers/campanha-data-builder.ts
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker'
 import { CampanhaProps } from '../../entities/campanha.entity'
+import { DomainRules } from '../../common/domain.rules'
 
 type Props = Partial<CampanhaProps>
 
@@ -8,13 +9,27 @@ export function CampanhaDataBuilder(props: Props): CampanhaProps {
     titulo: props.titulo ?? faker.word.words(4).substring(0, 40),
     descricao: props.descricao ?? faker.word.words(10).substring(0, 255),
     qtdBilhetesTotais:
-      props.qtdBilhetesTotais ?? faker.number.int({ min: 10, max: 99999 }),
-    qtdBilhetesComprados: props.qtdBilhetesComprados ?? 0,
+      props.qtdBilhetesTotais ??
+      faker.number.int({
+        min: DomainRules.MIN_BILHETES_TOTAIS,
+        max: DomainRules.MAX_BILHETES,
+      }),
+    qtdBilhetesComprados:
+      props.qtdBilhetesComprados ??
+      faker.number.int({
+        min: DomainRules.MIN_BILHETES_TOTAIS,
+        max: DomainRules.MAX_BILHETES,
+      }),
     dataInicioCampanha: props.dataInicioCampanha ?? new Date(),
     dataFimCampanha: props.dataFimCampanha ?? null,
     porcentagemProgresso: props.porcentagemProgresso ?? 0,
     campanhaAtiva: props.campanhaAtiva ?? true,
     valorUnitarioBilhete:
-      props.valorUnitarioBilhete ?? faker.number.float({ fractionDigits: 2 }),
+      props.valorUnitarioBilhete ??
+      faker.number.float({
+        fractionDigits: 2,
+        min: DomainRules.MIN_PROGRESS_PERCENTAGE,
+        max: DomainRules.MAX_PROGRESS_PERCENTAGE,
+      }),
   }
 }
